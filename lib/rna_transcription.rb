fa1f566ca40a83b95798e9f48ff36c22f0a039d9
Sweta Tiwari <swetavtiwari62189@gmail.com>
2019-01-11 00:30:30 +0530

    add cytosine to guanine conversion test

--- lib/rna_transcription.rb
+++ lib/rna_transcription.rb
@@ -1,3 +1,12 @@
 def dna_to_rna(dna)
-  ""
+  rna=""
+  dna.upcase.each_char  do |i|
+    case i
+    when 'C'
+      rna+='G'
+    else
+      next
+    end
+  end
+  rna
 end
--- spec/rna_transcription_spec.rb
+++ spec/rna_transcription_spec.rb
@@ -3,4 +3,7 @@ describe "dna_to_rna" do
   it "return empty string if input is empty" do
     expect(dna_to_rna("")).to eq ""
   end
+  it "complement of cytosine to guanine" do
+    expect(dna_to_rna("C")).to eq "G"
+  end
 end
